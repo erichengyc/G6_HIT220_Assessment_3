0d5b524e72c0675dbf02b1f4feef05d84a012bae
erichengyc <68725887+erichengyc@users.noreply.github.com>
2021-10-12 17:18:02 +0930

    copied the question to py files

---
+++ Question_1.py
@@ -0,0 +1,12 @@
+# Question 1 
+ 
+# To test the possibility of blocking route between two locations, the rangers have to perform an 
+# exhaustive search on the path options between the two locations. This will involve them moving 
+# between known sightings and along estuaries or shoreline to assess the ability for crocs to pass, 
+# or possible ways to block their passage. 
+
+# Your task is to devise an algorithm to determine the minimum cost of performing an exhaustive 
+# search between two points, where this cost is proportional to ground covered. Also give the list 
+# of locations that were chosen to segment the route in order to obtain this minimum cost. The 
+# cost is estimated just in units (how many units of work time required). 
+# Routine is computeCosting (location1, location 2).  
---
+++ Question_3.py
@@ -0,0 +1,11 @@
+# Question 3 	 
+
+# You are again asked to find the minimum distance between two locations in terms of number of metres 
+# and hence time for croc to travel. Croc speed is about 16 km/hr in water and 6km/hr on land. You are 
+# required to specify the route in terms of the points travelled through on the path. Method minTime() 
+# returns an array and a time value 
+
+# As an extension, provide the number of crocs in a certain radius of a beach. Using this array of 
+# locations, decide which is the optimum path segment between two points to insert a blockage 
+# that would  make the beach safer, by increasing the time the maximum number of crocs would 
+# have to travel. 
